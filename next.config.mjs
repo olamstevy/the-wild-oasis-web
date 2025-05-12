af31718dc9877e8293c0b8980ef24bcde43deb66
olamstevy <olamstevy@gmail.com>
2025-05-12 10:50:22 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "tbdtqrcnzuisszyeppqu.supabase.co",
+				pathname: "/storage/v1/object/public/cabin-images/**",
+			},
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+				pathname: "/**",
+			},
+		],
+	},
+	// output: "export",
+};
 
 export default nextConfig;
